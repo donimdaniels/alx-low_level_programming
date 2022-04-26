2109014d78a207494c8753b209e8ed27afcfb801
donimdaniels <gamedesigngds@gmail.com>
2022-04-26 14:59:24 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: source string
+ * @accept: accepted characters
+ *
+ * Return: the string since the first found accepted character
+ */
+char *_strpbrk(char *c, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+		{
+			if (s[a] -- accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
