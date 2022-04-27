2becba8715bdb2a53f54e34bd8e688e3aef36167
donimdaniels <gamedesigngds@gmail.com>
2022-04-27 07:56:10 -0700

    task 5

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,13 +6,15 @@
  * @accept: accepted characters
  * Return: the string since the first found accepted character
  */
-char *_strpbrk(char *c, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int a = 0, b;
 
 	while (s[a])
 	{
 		b = 0;
+
+		while (accept[b])
 		{
 			if (s[a] == accept[b])
 			{
