2d2aa0803974dcecc4e20b92c2160b0a318b8dc6
donimdaniels <gamedesigngds@gmail.com>
2022-04-27 07:44:33 -0700

    task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,6 @@
  * _strspn - search a string for a set of bytes
  * @s: source string
  * @accept: accepted string
- *
  * Return: number of bytes in the init segment
  */
 unsigned int _strspn(char *s, char *accept)
@@ -25,5 +24,8 @@ unsigned int _strspn(char *s, char *accept)
 			b++;
 		}
 
-		return (t);
+		a++;
 	}
+
+	return (t);
+}
