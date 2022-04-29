f35a684915fcdb8fbf9fd269e8002b6e30f2026f
donimdaniels <gamedesigngds@gmail.com>
2022-04-29 11:10:50 -0700

    task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string fololowed by a new line
+ * @s: string
+ * Return: Always (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
