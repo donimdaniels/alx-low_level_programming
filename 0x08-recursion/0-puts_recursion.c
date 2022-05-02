fc611896aae8eff3c4624cbb1b73b14d9d715dd7
donimdaniels <gamedesigngds@gmail.com>
2022-05-02 01:55:40 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,18 @@
 /**
  * _puts_recursion - print a string fololowed by a new line
  * @s: string
- * Return: Always (0)
+ * Return: void
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
 	{
-		_putchar(*s);
-		_puts_recursion(++s);
+		_putchar('\n');
+		return;
 	}
-	return (0);
+
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
 }
