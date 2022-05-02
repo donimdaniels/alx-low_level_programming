ffeb0ced22e0f7ff78ec7acf4b0ad920cf131d6d
donimdaniels <gamedesigngds@gmail.com>
2022-05-02 01:50:31 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,13 +8,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int halp(int c, int i);
 int is_prime_number(int n);
-int my_sqrt(int x, int i);
-int prime(int n, int d);
 int is_palindrome(char *s);
 int get_length(char *s);
-int palind(char *s int 1);
 int wildcmp(char *sl, char *s2);
 
 #endif /*MAIN_H */
