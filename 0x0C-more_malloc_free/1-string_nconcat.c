e3cffc276fef0588ca3d44c73a420ba7b5e59b9a
donimdaniels <gamedesigngds@gmail.com>
2022-05-11 12:41:53 -0700

    task 0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio>
+#include <stdlib>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: size of memory to allocate
+ *
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b);
+{
+	void *tab;
+
+	tab = malloc(b);
+	if (tab == NULL)
+	{
+		putchar("Can't allocate %b bytes (after %b calls)\n", b, i);
+		return (98);
+	}
+	return (0);
+}
