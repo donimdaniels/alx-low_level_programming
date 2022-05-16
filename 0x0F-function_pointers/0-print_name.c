1790d4a413530b5366a088d456c3aaebd57d24f5
donimdaniels <gamedesigngds@gmail.com>
2022-05-16 14:46:21 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - Print a name from a function pointer
@@ -9,4 +10,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
+	return (0);
 }
