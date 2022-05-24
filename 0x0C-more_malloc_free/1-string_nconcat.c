afb42d3eeabc5139a16466459221281d967a74ff
donimdaniels <gamedesigngds@gmail.com>
2022-05-24 07:42:01 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,55 @@
 #include "main.h"
-#include <stdio>
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	unsigned int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+
+}
 
 /**
- * malloc_checked - allocate memory
- * @b: size of memory to allocate
- *
- * Return: 0
+ * *string_nconcat - function that concatenates two strings
+ * @s1: type char
+ * @s2: type char
+ * @n: type unsigned int
+ * Return: NULL
  */
-void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *tab;
+	char *p;
+	unsigned int  len, x, y;
 
-	tab = malloc(b);
-	if (tab == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len = (unsigned int)_strlen(s1);
+	p = malloc((len + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0, y = 0; x < (len + n); x++)
 	{
-		putchar("Can't allocate %b bytes (after %b calls)\n", b, i);
-		return (98);
+		if (x < len)
+			*(p + x) = *(s1 + x);
+		else
+		{
+			*(p + x) = *(s2 + y);
+			y++;
+		}
 	}
-	return (0);
+	*(p + x) = '\0';
+
+	return (p);
 }
